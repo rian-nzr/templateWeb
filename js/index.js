34107ee0086b493271948298660ecc605ef37b8b
kaimju <90521244+rian-nzr@users.noreply.github.com>
2023-08-19 22:40:21 +0700

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,9 +1,12 @@
 var prevScrollpos = window.pageYOffset;
 window.onscroll = function () {
+    
     var currentScrollPos = window.pageYOffset;
+    
     if (prevScrollpos > currentScrollPos) {
         document.getElementById("navbar").style.top = "0";
-    } else {
+    } 
+    else {
         document.getElementById("navbar").style.top = "-50px";
     }
     prevScrollpos = currentScrollPos;
